204cd4a77918dfd966352e83d7301a6bba2839d3
trey <trey@fehuit.com>
2020-04-15 01:11:05 -0500

    added script to parse Wuhan PDFs and Download

---
+++ cn/runThisWithLInesEnding.ps1
@@ -0,0 +1,28 @@
+﻿$preStr = "https://ipfs.io/ipfs/bafykbzaced4xstofs4tc5q4irede6uzaz3qzcdvcb2eedxgfakzwdyjnxgohq/"
+
+$allLines = gc linesEnding
+
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+
+
+foreach($line in $allLines){
+    $compiledString = $preStr + $line
+    $compiledString 
+    
+    Invoke-WebRequest $compiledString 
+    pause
+}
