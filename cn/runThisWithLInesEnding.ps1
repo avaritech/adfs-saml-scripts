f9c7cd00d8f2d82edc0b5abc683374a49e91f652
trey <trey@fehuit.com>
2020-04-15 01:18:50 -0500

    fixed pause, added downloadDir

--- cn/runThisWithLInesEnding.ps1
+++ cn/runThisWithLInesEnding.ps1
@@ -1,5 +1,5 @@
 ﻿$preStr = "https://ipfs.io/ipfs/bafykbzaced4xstofs4tc5q4irede6uzaz3qzcdvcb2eedxgfakzwdyjnxgohq/"
-
+$downloadDir = "T:\CN-investigation\"
 $allLines = gc linesEnding
 
 add-type @"
@@ -17,12 +17,13 @@ add-type @"
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-
+$i=0
 
 foreach($line in $allLines){
+$i++
     $compiledString = $preStr + $line
     $compiledString 
     
-    Invoke-WebRequest $compiledString 
-    pause
+    Invoke-WebRequest $compiledString -outFile "$downloadDir$i.pdf"
+
 }
